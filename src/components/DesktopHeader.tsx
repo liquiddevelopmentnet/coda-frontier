e0b9362860237dd73a050e1335ec2c83b9cc4cec
liquiddevelopmentnet <info@minemix.de>
2022-09-07 18:10:58 +0200

    replaced discord's window controls with open source ones for copyright reasons

--- src/components/DesktopHeader.tsx
+++ src/components/DesktopHeader.tsx
@@ -3,6 +3,9 @@
  * All rights reserved.
  */
 
+import { CgClose } from 'react-icons/cg'
+import { GiSquare } from 'react-icons/gi'
+import { VscChromeMinimize } from 'react-icons/vsc'
 import { electronState } from '../recoil/atoms'
 import { useRecoilValue } from 'recoil'
 import { useTranslations } from '../i18n/i18n'
@@ -26,21 +29,7 @@ function DesktopHeader() {
               }}
               className='h-full flex w-7 hover:bg-slate-700 cursor-pointer'
             >
-              <svg
-                className='text-white m-auto pointer-events-none'
-                aria-hidden='false'
-                width='12'
-                height='12'
-                viewBox='0 0 12 12'
-              >
-                <rect
-                  fill='currentColor'
-                  width='10'
-                  height='1'
-                  x='1'
-                  y='6'
-                ></rect>
-              </svg>
+              <VscChromeMinimize color='white' className='m-auto' />
             </div>
             <div
               onClick={() => {
@@ -48,22 +37,7 @@ function DesktopHeader() {
               }}
               className='h-full flex w-7 hover:bg-slate-700 cursor-pointer'
             >
-              <svg
-                className='text-white m-auto'
-                aria-hidden='false'
-                width='12'
-                height='12'
-                viewBox='0 0 12 12'
-              >
-                <rect
-                  width='9'
-                  height='9'
-                  x='1.5'
-                  y='1.5'
-                  fill='none'
-                  stroke='currentColor'
-                ></rect>
-              </svg>
+              <GiSquare color='white' size={10} className='m-auto' />
             </div>
             <div
               onClick={() => {
@@ -71,19 +45,7 @@ function DesktopHeader() {
               }}
               className='h-full flex w-7 hover:bg-red-500 cursor-pointer'
             >
-              <svg
-                className='text-white m-auto'
-                aria-hidden='false'
-                width='12'
-                height='12'
-                viewBox='0 0 12 12'
-              >
-                <polygon
-                  fill='currentColor'
-                  fillRule='evenodd'
-                  points='11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1'
-                ></polygon>
-              </svg>
+              <CgClose color='white' className='m-auto' />
             </div>
           </div>
         </div>
