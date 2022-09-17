343fbc5867bbc8dfc65ccc40dc2d6c6940327f53
liquiddevelopmentnet <info@minemix.de>
2022-09-17 10:15:30 +0200

    discord rpc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+extern crate discord_rpc_client;
+
+use discord_rpc_client::Client;
+
 fn main() {
+
+  let mut drpc = Client::new(1017047174686715975).expect("Failed to create client");
+
+  drpc.start();
+
+  drpc.set_activity(|act| act.state("Idling"))
+      .expect("Failed to set activity");
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
