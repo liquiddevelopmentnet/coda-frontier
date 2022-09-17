08f879b4d7aaff2753fc9f2bb3fe808330ac2a4a
liquiddevelopmentnet <info@minemix.de>
2022-09-17 20:18:33 +0200

    discord rpc prototype

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,35 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 extern crate discord_rpc_client;
 
 use discord_rpc_client::Client;
+use std::time::SystemTime;
 
 fn main() {
+    let mut drcp: Client = Client::new(1017047174686715975).expect("Failed to create client");
+    drcp.start();
 
-  let mut drpc = Client::new(1017047174686715975).expect("Failed to create client");
+    if let Err(why) = drcp.set_activity(|a| {
+        a.state("Speed Programming 1v1")
+            .details("Ingame")
+            .timestamps(|t| {
+                t.start(
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis() as u64,
+                )
+            })
+            .assets(|a| a.large_image("lki").large_text("Coda"))
+            .instance(true)
+    }) {
+        println!("Failed to set presence: {}", why);
+    }
 
-  drpc.start();
-
-  drpc.set_activity(|act| act.state("Idling"))
-      .expect("Failed to set activity");
-
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
